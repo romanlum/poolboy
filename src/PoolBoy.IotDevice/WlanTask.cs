c3ccb4e1788d6d769234a468daf5c04e80a0ccd2
Roman Lumetsberger <roman.lumetsberger@outlook.com>
2022-05-07 16:07:47 +0200

    Added project and implemented wlan task to connect to preconfigured wifi

---
+++ src/PoolBoy.IotDevice/WlanTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Threading;
+using nanoFramework.Networking;
+
+namespace PoolBoy.IotDevice
+{
+    /// <summary>
+    /// Task for connecting to a wireless lan
+    /// </summary>
+    internal static class WLANTask
+    {
+        /// <summary>
+        /// Ip address 
+        /// </summary>
+        internal static string Ip {get; private set;}
+
+
+        /// <summary>
+        /// Error message if task fails
+        /// </summary>
+        internal static string ErrorMessage { get; private set; }
+
+        internal static bool Run()
+        {
+            CancellationTokenSource cs = new(10000);
+            var success = WiFiNetworkHelper.Reconnect(true, token: cs.Token);
+            if (!success)
+            {
+                // Something went wrong, you can get details with the ConnectionError property:
+                Debug.WriteLine($"Can't connect to the network, error: {WiFiNetworkHelper.Status}");
+                ErrorMessage = WiFiNetworkHelper.Status.ToString();
+                Ip = string.Empty;
+                return false;
+                
+            }
+
+            Ip = NetworkInterface.GetAllNetworkInterfaces()[0].IPv4Address;
+            return true;
+        }
+    }
+}
---
+++ src/PoolBoy.IotDevice/readme.md
@@ -0,0 +1,9 @@
+﻿# Development instructions
+
+Nanoframework has a tight coupled dependecy system, so the firmware and the packages have to match.
+
+To use this project you have to flash your esp32 with firmware `1.7.4`
+
+```
+nanoff.exe--update --platform esp32 --serialport COM5  --fwversion 1.7.4.0 --baud 1500000
+```
